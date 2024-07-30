2fb14646ba946c7c6b508042753183f2970ce0ed
ZRZ2233 <67986349+ZRZ2233@users.noreply.github.com>
2024-07-30 15:47:00 +0800

    add scope for ResourceSystemTags (#288)
    Co-authored-by: zhurenzhu.zrz <zhurenzhu.zrz@alibaba-inc.com>

--- client_tag.go
+++ client_tag.go
@@ -30,6 +30,7 @@ type ResourceTags struct {
 type ResourceSystemTags struct {
 	ResourceTags
 	TagOwnerUid string `json:"tagOwnerUid"`
+	Scope       string `json:"scope,omitempty"`
 }
 
 // ResourceUnTags tag for untag sls resouce
@@ -92,12 +93,12 @@ func NewResourceUnTags(resourceType string, resourceId string, tags []string) *R
 // NewResourceSystemTags create system tags for resource of certain type
 func NewResourceSystemTags(resourceType string, resourceId string, tagOwnerUid string, tags []ResourceTag) *ResourceSystemTags {
 	return &ResourceSystemTags{
-		ResourceTags{
+		ResourceTags: ResourceTags{
 			ResourceType: resourceType,
 			ResourceID:   []string{resourceId},
 			Tags:         tags,
 		},
-		tagOwnerUid,
+		TagOwnerUid: tagOwnerUid,
 	}
 }
 
